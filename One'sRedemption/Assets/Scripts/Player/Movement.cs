74ae14685873cd71b0eabf816b40fc1ac6ee2a8e
MatiasCarini <matias.carini@davinci.edu.ar>
2022-04-25 16:34:35 -0300

    movemenfixed

--- One'sRedemption/Assets/Scripts/Player/Movement.cs
+++ One'sRedemption/Assets/Scripts/Player/Movement.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 
 public class Movement
 {
-    Transform    _transform;
-    Rigidbody    _rb;
-    LayerMask    _layerMask;
-    float        _moveSpeed,
+    Transform _transform;
+    Rigidbody _rb;
+    LayerMask _layerMask;
+    float _moveSpeed,
                  _jumpForce,
                  _smoothVelocity,
                  _smoothTime;
-    Walking      _walking;
+
     public Movement(Transform t, Rigidbody rb, float s, float jf,LayerMask _layerMask)
     {
         _transform = t;
         _rb = rb;
         _moveSpeed = s;
         _jumpForce = jf;
-
         _smoothTime = 0.05f;
-        _walking = new Walking();
+
         
     }
     public void MovePlayer(Vector3 inputVector)
@@ -30,7 +29,11 @@ public class Movement
         {
             Player.instance.moving = true; //Pj en movimiento
 
-            _walking.Walk( inputVector,_transform,_smoothTime,_smoothVelocity,_moveSpeed,_rb);
+            Vector3 dir = new Vector3(inputVector.x, 0, inputVector.z).normalized;
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetAngle, ref _smoothVelocity, _smoothTime);
+            _transform.rotation = Quaternion.Euler(0, angle, 0);
+            _rb.MovePosition(_rb.position + dir * _moveSpeed * Time.deltaTime);
         }
         else
         {
