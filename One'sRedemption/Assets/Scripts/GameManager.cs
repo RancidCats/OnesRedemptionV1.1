1107453f7f79e8302ab20a7fe6c8c46e6c0b2e8b
Matonini <102761279+Matonini@users.noreply.github.com>
2022-04-30 14:46:37 -0300

    Managers&Arreglos

---
+++ One'sRedemption/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    //public GameObject protagonist;
+    //
+    //public GameObject VictoryScreen, DefeatScreen;
+
+    public float timer, startTime;
+
+    public bool victoryflag = true;
+
+    //public List<GameObject> enemies = new List<GameObject>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Quick reload of the scene
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        //If protagonist is dead timer stops and shows defeat screen
+        
+        //if (Player.instance)
+        //    timer -= Time.deltaTime;
+        //else
+        //    DefeatScreen.SetActive(true);
+
+        //This is just to delay the win function until all the enemies are added to the list
+        
+        //if (startTime >= 2)
+        //    Win();
+        //else
+        //    startTime += Time.deltaTime;
+    }
+
+    //Win function
+    //public void Win()
+    //{
+    //    if (enemies.Count == 0 && victoryflag)
+    //    {
+    //        VictoryScreen.SetActive(true);
+    //        print("YOU DEFEATED ALL THE ENEMIES, CONGRATULATIONS");
+    //        victoryflag = false;
+    //    }
+    //}
+}
